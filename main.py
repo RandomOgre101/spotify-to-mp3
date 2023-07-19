b32abc5cd3e5ef2280e4498c9fb3c34defd01077
rishikeshavravi2020 <daxsteel1234@gmail.com>
2023-07-19 23:29:23 +0530

    added spotify + yt links

--- main.py
+++ main.py
@@ -1,18 +1,69 @@
-from bs4 import BeautifulSoup
+from googleapiclient.discovery import build
+from dotenv import load_dotenv
+import os
 import requests
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+import base64
+import json
+load_dotenv()
 
-CLIENT_ID = "--"
-CLIENT_SECRET = "--"
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+YOUTUBE_API = os.getenv("YOUTUBE_API")
 
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri="https://example.com",
-        scope="playlist-modify-private",
+
+def get_token():
+    auth_string = CLIENT_ID + ":" + CLIENT_SECRET
+    auth_bytes = auth_string.encode('utf-8')
+    auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
+
+    URL = 'https://accounts.spotify.com/api/token'
+    headers = {
+        "Authorization": 'Basic '+auth_base64,
+        "Content-Type": 'application/x-www-form-urlencoded'
+    }
+    data = {"grant_type": "client_credentials"}
+    result = requests.post(URL, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+
+token = get_token()
+
+playlist_id = str(input("Enter the playlist link: ").split('/')[-1].strip())
+
+headers = {
+    "Authorization": "Bearer "+token
+}
+
+response = requests.get(f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks',headers=headers)
+data = response.json()
+
+song_list = []
+artist_list = []
+total_songs = int(data["total"])
+
+for num in range(total_songs):
+    song_list.append(data["items"][num]["track"]["name"])
+    artist_list.append(data["items"][num]["track"]["artists"][0]["name"])
+
+songs = list(zip(song_list, artist_list))
+
+
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API)
+links = []
+for song in songs:
+    req = youtube.search().list(
+        part = "id",
+        q = f'{song[0]} - {song[1]}',
+        maxResults = 1
     )
-)
-user = sp.current_user()
-user_id = user["id"]
+    yt = req.execute()
+    etag = str(yt["items"][0]["id"]["videoId"])
+    youtube_link = 'https://www.youtube.com/watch?v='+etag
+    links.append(youtube_link)
+
+
+
+
+
